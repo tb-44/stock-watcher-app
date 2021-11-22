e96c38cf5b7ae6e5b64bc0a0aa54eb35a3e3508c
Calvin Juárez <calvin@foreup.com>
2021-11-22 15:41:51 -0700

    StockQuoteCard:js – make it more Marionette-ish
    also improve code standards

--- src/StockQuote/StockQuoteView.js
+++ src/StockQuote/StockQuoteView.js
@@ -1,23 +1,20 @@
 import Marionette from 'backbone.marionette';
 import template from './StockQuoteCard.html';
 import _ from 'underscore';
-import './stock-quote-card.scss'
+import './stock-quote-card.scss';
 
 export const StockQuoteView = Marionette.View.extend({
-    className: "stock-card",
+    tagName: 'section',
+    className: 'stock-card',
     template: _.template(template),
 
-    initialize: function() {
-        _.bindAll(this, 'template');
+    templateContext() {
+        return {
+            /**
+             * @param {number|string} value
+             * @returns {*}
+             */
+            formatMoney: value => value, // @TODO - complete the format money method, don't just return the input.
+        };
     },
-
-    /**
-     *
-     * @param {String} value
-     * @returns {*}
-     */
-    formatMoney: function(value) {
-        // TODO - complete the format money method, don't just return the input.
-        return value
-    }
-})
+});
